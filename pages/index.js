57bfffc67bffb595a4fb22cc3491c94b078bbf65
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-10 10:03:31 +0200

    add h2 description of vercel bad deployment

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Exercise4 from '../components/Exercise4';
 export default function IndexPage() {
   return (
     <div>
+    <h2> This project can not make a connection to the JSON Database file uploaded with this project. The Apis can not get to that path. But the Code is valid and locally this project works all fine. </h2>
       <Exercise1 />
       <hr />
       <Exercise2 />
